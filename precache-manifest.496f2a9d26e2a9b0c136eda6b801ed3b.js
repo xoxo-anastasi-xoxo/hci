f5c9cd0dacf7a088a4ad53e5817a5004e4785f23
arkazantseva <arkazantseva@yandex-team.ru>
2019-12-23 00:21:06 +0300

    Updates

---
+++ precache-manifest.496f2a9d26e2a9b0c136eda6b801ed3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cfae2a564a9056af9e04c9bd58a453e",
+    "url": "/hci/index.html"
+  },
+  {
+    "revision": "1e63e83570333d8002b3",
+    "url": "/hci/static/css/2.1842d3f4.chunk.css"
+  },
+  {
+    "revision": "2d9081c20be277b961b6",
+    "url": "/hci/static/css/main.4313236e.chunk.css"
+  },
+  {
+    "revision": "1e63e83570333d8002b3",
+    "url": "/hci/static/js/2.10e242b9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/hci/static/js/2.10e242b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d9081c20be277b961b6",
+    "url": "/hci/static/js/main.c5caa7e1.chunk.js"
+  },
+  {
+    "revision": "08d896b267bd8bfefcea",
+    "url": "/hci/static/js/runtime-main.033c6918.js"
+  }
+]);
